1fe8334cc2ce8c2cf6ee0086bec4c8b9a0eb6895
ahmad-irfaan <irfaan.hibatullah@gmail.com>
2021-06-19 16:18:49 +0700

    Create Title, Logo, and Background Color

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
-import pygame
+import pygame  # membutuhkan packages atau library pygame
 
 # Initialize the pygame
 pygame.init()
 
 # membuat tampilan windows pada prgoram
 screen = pygame.display.set_mode((1000, 600))
+icon = pygame.image.load('ufo.png')
+pygame.display.set_icon(icon)
 
-#membuat variabel untuk infinite loop sehingga tampilan game tetap menyala
+# Title and Icon
+pygame.display.set_caption("Game Python DTS ProA-Academy Space Invades")
+
+# membuat variabel untuk infinite loop sehingga tampilan game tetap menyala
 running = True
 while running:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: #membuat pengondisian untuk agar program
-            running = False             #keluar ketika user klik sebuah button
+        if event.type == pygame.QUIT:  # membuat pengondisian untuk agar program
+            running = False  # keluar ketika user klik sebuah button
+
+    # RGB - Red Green and Blue untuk background gambar screen-nya
+    screen.fill((0, 0, 0))
+    pygame.display.update()
