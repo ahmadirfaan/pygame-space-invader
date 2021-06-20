453b98782013a6166b63f81cc872552fc4db7c68
ahmad-irfaan <irfaan.hibatullah@gmail.com>
2021-06-21 02:05:31 +0700

    Add Game Over Text and Condition

--- main.py
+++ main.py
@@ -51,16 +51,24 @@ bulletX_change = 4
 bulletY_change = 10
 bullet_state = "ready"
 
-#membuat skor dan meletakkan teks skor dalam tmapilan
+# membuat skor dan meletakkan teks skor dalam tmapilan
 score_value = 0
 font = pygame.font.SysFont('freesandsbold.tff', 32)
 textX = 10
 textY = 10
 
+# Game Over
+game_over_font = pygame.font.SysFont('freesandsbold.tff', 64)
+
 
 def show_score(x, y):
     score = font.render("Score : " + str(score_value), True, (255, 255, 255))
     screen.blit(score, (x, y))
+
+
+def game_over_text():
+    game_over_display_text = game_over_font.render("GAME OVER", True, (255, 255, 255))
+    screen.blit(game_over_display_text, (250, 250))
 
 
 def player(x, y):
@@ -125,12 +133,20 @@ while running:
 
     # Menambahkan kondisi untuk membuat batas tampilan enemy ke window
     for i in range(num_of_enemies):
+
+        # Game Over
+        if enemyY[i] > 140:
+            for j in range(num_of_enemies):
+                enemyY[j] = 2000
+            game_over_text()
+            break
+
         enemyX[i] += enemyX_change[i]
         if enemyX[i] <= 0:
-            enemyX_change[i] = 4
+            enemyX_change[i] = 3
             enemyY[i] += enemyY_change[i]
         elif enemyX[i] >= 736:
-            enemyX_change[i] = -4
+            enemyX_change[i] = -3
             enemyY[i] += enemyY_change[i]
 
         # Collision
