0c7b35523f99a5ba1ba62e9c9ba6248d0ab4c2fa
ahmad-irfaan <irfaan.hibatullah@gmail.com>
2021-06-20 23:43:58 +0700

    Adding movement mechanics to enemy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame  # membutuhkan packages atau library pygame
+import random
 
 # Initialize the pygame
 pygame.init()
@@ -17,9 +18,20 @@ playerX = 370
 playerY = 380
 playerX_change = 0
 
+# Membuat lawan
+enemyImg = pygame.image.load('enemy.png')
+enemyX = random.randint(0, 800)
+enemyY = random.randint(50, 150)
+enemyX_change = 0.3
+enemyY_change = 40
+
 
 def player(x, y):
     screen.blit(playerImg, (x, y))
+
+
+def enemy(x, y):
+    screen.blit(enemyImg, (x, y))
 
 
 # membuat variabel untuk infinite loop sehingga tampilan game tetap menyala
@@ -28,11 +40,38 @@ while running:
 
     # Red, Green, Blue
     screen.fill((0, 0, 0))
-    playerX += 0.1
     for event in pygame.event.get():
         if event.type == pygame.QUIT:  # membuat pengondisian untuk agar program
             running = False  # keluar ketika user klik sebuah button
 
-    # RGB - Red Green and Blue untuk background gambar screen-nya
+        # if keystroke is pressed check whether its right or left
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.3
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 0.3
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
+
+    # Menambahkan kondisi untuk membuat batas tampilan player ke window
+    playerX += playerX_change
+    if playerX <= 0:
+        playerX = 0
+    elif playerX >= 736:
+        playerX = 736
+
+    # Menambahkan kondisi untuk membuat batas tampilan enemy ke window
+    enemyX += enemyX_change
+
+    if enemyX <= 0:
+        enemyX_change += 0.3
+        enemyY += enemyY_change
+    elif enemyY >= 736:
+        enemyX_change += 0.3
+        enemyY += enemyY_change
+
+
     player(playerX, playerY)  # memanggil method player
+    enemy(enemyX, enemyY)  # memanggil method enemy
     pygame.display.update()
